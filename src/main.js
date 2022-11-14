dc2d265a42a276a00e50ec625ab527df509dc22a
Aen Jojo <josuafernando999@gmail.com>
2022-11-14 13:39:39 +0700

    make an easier code to change day

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+const day = 6;
+const challenge = 2;
+
 const path = require('path');
-const solution = require('./solution/aoc_d6_c2');
+const solution = require(`./solution/aoc_d${day}_c${challenge}`);
 const readline = require('./module/readline');
 
 // solution();
 
-const inputPath = path.resolve(__dirname, './input/input_d6.txt');
+const inputPath = path.resolve(__dirname, `./input/input_d${day}.txt`);
 
 readline(inputPath, solution);
